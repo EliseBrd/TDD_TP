2371a3ed8bfb0d2c24df87ecd1d35d87fd9b5c81
Elise-44 <bouardelise.perso@gmail.com>
2025-02-26 16:10:36 +0100

    init project

---
+++ src/main/java/tdd/elise/tp/TpFinalTddApplication.java
@@ -0,0 +1,13 @@
+package tdd.elise.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpFinalTddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TpFinalTddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tdd/elise/tp/TpFinalTddApplicationTests.java
@@ -0,0 +1,13 @@
+package tdd.elise.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpFinalTddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
